e1d804bddaa34636d36ebff2f9b3e2e78a0aee5f
Smaran Kapa 2021 <skapa21@3012-l29531-s.local>
2019-05-13 12:16:27 -0500

    Initial Commit

---
+++ WelcomeWagon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WelcomeWagon
+//
+//  Created by Smaran Kapa 2021 on 5/13/19.
+//  Copyright © 2019 Smaran Kapa 2021. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
